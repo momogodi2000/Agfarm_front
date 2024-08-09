fe3cd189f7d34c4af0bc2ac73c8d78cd6e286b64
Ange-Sepdeu <chriskameni25@gmail.com>
2024-08-09 12:20:08 +0000

    Modified auths and axiosConfig

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -18,14 +18,14 @@ const SignIn = () => {
     axios
       .post("/api/signin/", { username, password })
       .then((response) => {
-        const role = response.data.role;
+        const role = response.data.role?.toLowerCase();
         console.log(role);
         console.log("login successfully");
         if (role === "farmer") navigate("/farmer-dashboard");
-        else if (role === "Buyer") navigate("/buyer-dashboard");
-        else if (role === "Administrator") navigate("/admin-dashboard");
+        else if (role === "buyer") navigate("/buyer-dashboard");
+        else if (role === "administrator") navigate("/admin-dashboard");
         else if (role === "Financial Institutions")navigate("/financial-dashboard");
-        else if (role === "Government") navigate("/government-dashboard");
+        else if (role === "government") navigate("/government-dashboard");
         else if (role === "Logistics Provider") navigate("/logistics-dashboard");
       })
       .catch(() => {
